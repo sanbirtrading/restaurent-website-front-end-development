9177920b97f1a044ce2673d036dcc270ccb7a3a6
sanbirtrading <sanbirtrading2016@gmail.com>
2022-07-08 18:24:14 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Select element funtion,
+const selectElement = function (element){
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement ('body');
+
+menuToggler.addEventListener('click', function() {
+    body.classList.toggle('open');
+})
+
+// Scroll reveal
+window.sr = ScrollReveal();
+
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300,
+});
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+sr.reveal('.animate-right0', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
+sr.reveal('.animate-left0', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600,
+});
